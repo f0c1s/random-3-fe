e8d81eb19f85d1a00dd8a85eb0a09ac6cba688b3
f0c1s <f0c1s>
2019-02-09 13:20:41 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -9,28 +9,15 @@ class App extends Component {
         super(props)
         this.state = {
             gridDimensions: { height: 10, width: 10 },
-            marioPosition: { x: 0, y: 0 },
-            mushrooms: []
+            marioPosition: { x: 0, y: 0 }
         }
         this.onCreateGrid = this.onCreateGrid.bind(this)
-        this.createRandomMushrooms = this.createRandomMushrooms.bind(this)
     }
     onCreateGrid(dimensions) {
         this.setState({ gridDimensions: dimensions })
         this.createRandomMushrooms()
     }
-    createRandomMushrooms() {
-        let shrooms = []
-        for (let i = 0; i < 10; i++) {
-            const x = Math.floor(Math.random() * this.state.gridDimensions.width)
-            const y = Math.floor(Math.random() * this.state.gridDimensions.height)
-            shrooms.push({ x, y })
-        }
-        this.setState({ mushrooms: shrooms })
-    }
-    componentDidMount() {
-        this.createRandomMushrooms()
-    }
+
     render() {
         return (
             <div className="App">
@@ -39,7 +26,7 @@ class App extends Component {
                 </header>
                 <section className="App-content">
                     <Input onCreateGrid={this.onCreateGrid} />
-                    <Grid dimensions={this.state.gridDimensions} mushrooms={this.state.mushrooms}/>
+                    <Grid dimensions={this.state.gridDimensions} />
                 </section>
                 {/* <footer className="App-footer">
                     <img src={logo} className="App-logo" alt="logo" />
--- src/Grid.js
+++ src/Grid.js
@@ -8,13 +8,14 @@ export default class Grid extends Component {
         this.state = {
             height: this.props.dimensions.height,
             width: this.props.dimensions.width,
-            mushrooms: this.props.mushrooms,
+            mushrooms: [],
             direction: null,
             timer: null,
             mario: {
                 next: { x: 0, y: 0 },
                 prev: { x: 0, y: 0 }
-            }
+            },
+            moves: 0
         }
         this.moveMario = this.moveMario.bind(this)
         this.includes = this.includes.bind(this)
@@ -22,6 +23,7 @@ export default class Grid extends Component {
         this.shouldBounce = this.shouldBounce.bind(this)
         this.changeDirection = this.changeDirection.bind(this)
         this.nextMarioLocation = this.nextMarioLocation.bind(this)
+        this.createRandomMushrooms = this.createRandomMushrooms.bind(this)
         ArrowKeysReact.config({
             left: () => {
                 console.log('going left')
@@ -50,6 +52,7 @@ export default class Grid extends Component {
         })
     }
     componentDidMount() {
+        this.createRandomMushrooms()
         const timer = setInterval(() => {
             this.moveMario()
         }, 100)
@@ -61,7 +64,19 @@ export default class Grid extends Component {
     static getDerivedStateFromProps(props, state) {
         return { height: props.dimensions.height, width: props.dimensions.width, mushroom: props.mushrooms }
     }
+    createRandomMushrooms() {
+        let shrooms = []
+        for (let i = 0; i < 10; i++) {
+            const x = Math.floor(Math.random() * this.state.width)
+            const y = Math.floor(Math.random() * this.state.height)
+            shrooms.push({ x, y })
+        }
+        this.setState({ mushrooms: shrooms })
+    }
     moveMario() {
+        if(this.state.mushrooms.length === 0) {
+            clearInterval(this.state.timer)
+        }
         if (this.state.direction) {
             let bounced = false
             if (this.shouldBounce(this.state.mario.prev.x, this.state.mario.prev.y)) {
@@ -71,11 +86,13 @@ export default class Grid extends Component {
             }
             console.log('move mario')
             const next = this.nextMarioLocation()
-            if(!bounced) {
+            if (!bounced) {
                 this.setState({ mario: { next: next, prev: this.state.mario.next } })
             } else {
                 this.setState({ mario: { next: next, prev: this.state.mario.prev } })
             }
+            const newMushrooms = this.state.mushrooms.filter(m => !(m.x === next.x && m.y === next.y))
+            this.setState({ mushrooms: newMushrooms, moves: this.state.moves + 1 })
             this.forceUpdate()
         }
     }
@@ -95,22 +112,22 @@ export default class Grid extends Component {
     }
 
     shouldBounce(x, y) {
-        if(x === 0 && y === 0 && (this.state.direction === 'u' || this.state.direction === 'l')) {
+        if (x === 0 && y === 0 && (this.state.direction === 'u' || this.state.direction === 'l')) {
             return true
         }
-        if(x === this.state.width && y === this.state.height && (this.state.direction === 'd' || this.state.direction === 'r')) {
+        if (x === this.state.width && y === this.state.height && (this.state.direction === 'd' || this.state.direction === 'r')) {
             return true
         }
-        if(x === 0 && this.state.direction === 'l') {
+        if (x === 0 && this.state.direction === 'l') {
             return true
         }
-        if(x === this.state.width -1 && this.state.direction === 'r') {
+        if (x === this.state.width - 1 && this.state.direction === 'r') {
             return true
         }
-        if(y === 0 && this.state.direction === 'u') {
+        if (y === 0 && this.state.direction === 'u') {
             return true
         }
-        if(y === this.state.height -1 && this.state.direction === 'd') {
+        if (y === this.state.height - 1 && this.state.direction === 'd') {
             return true
         }
         return false
@@ -133,9 +150,9 @@ export default class Grid extends Component {
         this.setState({ grid: grid })
     }
     includes(x, y) {
-        if (this.props.mushrooms[0]) {
+        if (this.state.mushrooms[0]) {
             for (let i = 0; i < this.state.width; i++) {
-                if (this.props.mushrooms[i].x === x && this.props.mushrooms[i].y === y) {
+                if (this.state.mushrooms[i] && this.state.mushrooms[i].x === x && this.state.mushrooms[i].y === y) {
                     return true
                 }
             }
@@ -150,10 +167,11 @@ export default class Grid extends Component {
             for (let i = 0; i < this.state.height; i++) {
                 grid[i] = [] // visual grid
                 for (let j = 0; j < this.state.width; j++) {
-                    if (i === this.state.mario.next.y && j === this.state.mario.next.x) {
+                    const isMarioThere = i === this.state.mario.next.y && j === this.state.mario.next.x
+                    if (isMarioThere) {
                         grid[i].push(item(`${i}-${j}`, false, "grid-item mario"))
                     } else {
-                        const hasMushroom = this.includes(j, i)
+                        const hasMushroom = this.includes(j, i) && !isMarioThere
                         grid[i].push(item(`${i}-${j}`, hasMushroom))
                     }
                 }
@@ -164,6 +182,9 @@ export default class Grid extends Component {
         return (
             <div id="grid" {...ArrowKeysReact.events} tabIndex="1">
                 {createGrid()}
+                {
+                    this.state.moves > 0 && this.state.moves + " moves"
+                }
             </div>
         )
     }
